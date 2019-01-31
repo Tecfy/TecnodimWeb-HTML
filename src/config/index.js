834a6ec1fc7d4f08c46d6e735381a0535de304d7
Rudolf Zanoni <rudolf@devplace.com.br>
2019-01-31 18:28:55 -0200

    [TEC] Ajustes

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,11 @@
 export default {
-    apiUrl: 'http://adm.tecnodimweb.tecfy.com.br/api',
-    externalLoginUlr: 'https://technodimweb-dev.tecfy.com.br',
-    externalLoginUlrRedirect: 'http://localhost:8080/login',
-    // externalLoginUlrRedirect: 'http://adm.tecnodimweb.tecfy.com.br/login',
-    // apiUrl: 'https://gedadminhomol.sereduc.com/api'
-    // apiUrl: 'https://gedadmin.sereduc.com/api'
+    //apiUrl: 'http://adm.tecnodimweb.tecfy.com.br/api',
+	//apiUrl: 'https://gedadminhomol.sereduc.com/api',
+	apiUrl: 'https://gedadmin.sereduc.com/api',
+    //externalLoginUlr: 'https://adm.tecnodimweb.tecfy.com.br',
+    //externalLoginUlr: 'https://gedadminhomol.sereduc.com',
+	apiUrl: 'https://gedadmin.sereduc.com',
+    //externalLoginUlrRedirect: 'http://tecnodimweb.tecfy.com.br/login',
+    //externalLoginUlrRedirect: 'http://gedhomol.sereduc.com/login',
+    externalLoginUlrRedirect: 'http://ged.sereduc.com/login',
 }
