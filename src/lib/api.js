5af55508dc9be9ee526efedb8d34d940ccbacded
renato.dourado <0011992200>
2019-04-29 16:44:44 -0300

    Correções API

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,45 +3,52 @@ import swal from 'sweetalert2';
 import config from '../config';
 
 function getToken() {
-    if (typeof window.localStorage.token === "undefined") {
-        // return "";
-        window.location.href = '/login';
-    }
-    return window.localStorage.token;
+  if (typeof window.localStorage.token === "undefined") {
+    // return "";
+    window.location.href = '/login';
+  }
+  return window.localStorage.token;
 }
 
 const instance = axios.create({
-    baseURL: config.apiUrl,
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + getToken(),
-    },
-    timeout: 100*36,
+  baseURL: config.apiUrl,
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + getToken(),
+  },
+  timeout: 60 * 1000 * 1000,
 });
 
 instance.interceptors.response.use(function (response) {
-    return response;
+  return response;
 }, function (error) {
+  if (error.response !== null) {
     if (error.response.status === 401) {
-        // localStorage.removeItem('illpy_user');
-        window.location.href = '/login';
-        return Promise.reject(error);
+      window.location.href = '/login';
+      return Promise.reject(error);
     }
     let msg = error.response.data.hasOwnProperty('msg') ? error.response.data.msg : error.response.data.message
     if (error.response.status === 422 && error.response.data.errors) {
-        msg = error.response.data.errors[Object.keys(error.response.data.errors)[0]];
+      msg = error.response.data.errors[Object.keys(error.response.data.errors)[0]];
     }
-
     swal({
-        type: 'error',
-        text: msg || 'Falha ao processar a requisição!',
-        showConfirmButton: false,
-        toast: true,
-        timer: 3000
-    })
-
-    return Promise.reject(error);
+      type: 'error',
+      text: msg || 'Falha ao processar a requisição!',
+      showConfirmButton: false,
+      toast: true,
+      timer: 3000
+    });
+  } else {
+    swal({
+      type: 'error',
+      text: 'Falha ao processar a requisição!',
+      showConfirmButton: false,
+      toast: true,
+      timer: 3000
+    });
+  }
+  return Promise.reject(error);
 });
 
 export default instance;
